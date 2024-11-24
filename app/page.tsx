b269b26fc16bbec376d4ce7f6ce14f6cbf2d99ad
Kerso <j.stankowski@sp14.elodz.edu.pl>
2024-11-24 10:31:27 +0100

    Site, just site ;3

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,64 @@
-import Image from "next/image";
+'use client'
+import { Typewriter } from 'react-simple-typewriter'
+import { FaDiscord } from "react-icons/fa6";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+
+    return (
+        <>
+            <div
+                className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+                <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+                    <h1 className="text-7xl font-semibold" id="typewrite"><Typewriter words={['KERSO']} typeSpeed={70}/>
+                    </h1>
+                    <p className="text-4xl font-semibold"><Typewriter words={['App developer']} delaySpeed={2000}/></p>
+                    <p className="text-4xl font-bold text-center">↓</p>
+                </main>
+            </div>
+            <div
+                className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+                <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+                    <h1 className="text-5xl">I can code in <Typewriter words={['HTML', 'JAVASCRIPT', 'C#']} cursor
+                                                                       typeSpeed={70} deleteSpeed={70} loop/></h1>
+                </div>
+            </div>
+            <div
+                className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+                <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+                    <h1 className="text-5xl font-semibold">My projects: </h1>
+                    <ul className="max-w-md space-y-1 text-gray-500 list-none list-inside dark:text-gray-400">
+                        <li>
+                            <b>&quot;KERSO.DEV v3&quot;</b> This site (this is not on github yet)
+                        </li>
+                        <li>
+                            <a href="https://github.com/Kerso-official/MpkCheck-v3"><b>&quot;MpkCheck&quot;</b></a> Web app for checking bus stop schedule online
+                        </li>
+                        <li>
+                            <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0"><b>&quot;QuizySP14&quot;</b> <i>Is not out yet ;3</i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div
+                className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-14 p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+                <i className="text-xl">Copyright &copy; Kerso {new Date().getFullYear()}</i>
+                <div
+                    className="row-start-3 flex gap-6 flex-wrap items-center justify-center font-bold text-xl">
+                    <a className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                       href="https://github.com/Kerso-official">
+                        <i>Github</i>
+                    </a>
+                    <a className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                       href="https://discord.com/users/1147582805803532478">
+                        <i>Discord</i>
+                    </a>
+                    <a className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                       href="mailto:kerso@kerso.dev">
+                        <i>Email</i>
+                    </a>
+                </div>
+            </div>
+        </>
+    );
 }
